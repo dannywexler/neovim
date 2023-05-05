41c285568236d3413361ae555d8701137dc7ce7c
Danny Wexler <dannywexler@gmail.com>
2023-05-05 00:45:13 -0700

    v2 of virtualerrors

--- lua/virtualerrors.lua
+++ lua/virtualerrors.lua
@@ -4,6 +4,7 @@ local aucmd = api.nvim_create_autocmd
 local virtErrorsNamespace = api.nvim_create_namespace('virtErrors')
 local space = ' '
 local warningIcon = ' '
+local maxDiagnostics = 100
 
 local highlight_groups = {
     [vim.diagnostic.severity.ERROR] = "DiagnosticVirtualTextError",
@@ -11,10 +12,6 @@ local highlight_groups = {
     [vim.diagnostic.severity.INFO] = "DiagnosticVirtualTextInfo",
     [vim.diagnostic.severity.HINT] = "DiagnosticVirtualTextHint",
 }
-
-local function clearVirtErrorNamespace()
-    api.nvim_buf_clear_namespace(0, virtErrorsNamespace, 0, -1)
-end
 
 local function log(item)
     if type(item) == 'table' then
@@ -28,19 +25,57 @@ local function getDiagnostics()
     local allDiagnostics = vim.diagnostic.get(0, {
         -- severity = vim.diagnostic.severity.ERROR
     })
-    if vim.tbl_isempty(allDiagnostics) then return end
+    if vim.tbl_isempty(allDiagnostics) or vim.tbl_count(allDiagnostics) > maxDiagnostics then return end
+
+    local lineDiagnosticsMap = {}
+
     for _, diagnostic in ipairs(allDiagnostics) do
-        -- log(diagnostic.message)
-        api.nvim_buf_set_extmark(0, virtErrorsNamespace, diagnostic.lnum, 0, {
-            virt_lines = {
+        if lineDiagnosticsMap[diagnostic.lnum] == nil then
+            lineDiagnosticsMap[diagnostic.lnum] = {}
+        end
+        table.insert(lineDiagnosticsMap[diagnostic.lnum], diagnostic)
+    end
+    -- log(lineDiagnosticsMap)
+
+    for lineNumber, diagGroup in pairs(lineDiagnosticsMap) do
+        table.sort(diagGroup, function(a, b) return a.col < b.col end)
+        local virtLines = {}
+        local firstLinePieces = {}
+
+        local endColOfLastPiece = 0
+        for index, diagnostic in ipairs(diagGroup) do
+            local paddingWidth = diagnostic.col - endColOfLastPiece
+            table.insert(firstLinePieces, {
+                space:rep(paddingWidth),
+                'CursorLine'
+            })
+            local diagnosticWidth = math.max(1, diagnostic.end_col - diagnostic.col)
+            table.insert(firstLinePieces, {
+                string.rep(index, diagnosticWidth),
+                highlight_groups[diagnostic.severity]
+            })
+            endColOfLastPiece = diagnostic.col + diagnosticWidth
+        end
+        table.insert(firstLinePieces, {
+            space:rep(vim.fn.winwidth(0) - endColOfLastPiece),
+            'CursorLine'
+        })
+
+        table.insert(virtLines, firstLinePieces)
+
+        for index, diagnostic in ipairs(diagGroup) do
+            local formattedMessage = '  ' .. index .. '. ' .. diagnostic.message
+            local remainingLength = math.max(1, vim.fn.winwidth(0) - #formattedMessage)
+            formattedMessage = formattedMessage .. space:rep(remainingLength)
+            table.insert(virtLines, {
                 {
-                    {
-                        -- space:rep(diagnostic.col) .. warningIcon .. ' ' .. diagnostic.message .. ' ' .. warningIcon,
-                        space:rep(2) .. warningIcon .. ' ' .. diagnostic.message .. ' ' .. warningIcon,
-                        highlight_groups[diagnostic.severity]
-                    },
-                }
-            },
+                    formattedMessage,
+                    highlight_groups[diagnostic.severity]
+                },
+            })
+        end
+        api.nvim_buf_set_extmark(0, virtErrorsNamespace, lineNumber, 0, {
+            virt_lines = virtLines,
         })
     end
 end
@@ -49,7 +84,7 @@ aucmd('CursorHold', {
     pattern = { '*.lua', '*.ts' },
     group = virtErrorsGroup,
     callback = function()
-        clearVirtErrorNamespace()
+        api.nvim_buf_clear_namespace(0, virtErrorsNamespace, 0, -1)
         getDiagnostics()
     end
 })
