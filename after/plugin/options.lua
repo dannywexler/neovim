d06735a1d5781b657ed6ea486f5e766f79f825d2
Danny Wexler <dannywexler@gmail.com>
2023-06-03 11:29:29 -0700

    simplified options to use more of the defaults

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -5,46 +5,42 @@ o.background = 'dark'
 o.backup = false
 o.clipboard = 'unnamedplus'
 o.cmdheight = 1
-o.compatible = false
+-- o.compatible = false
 o.cursorline = true
--- o.cursorcolumn = true
--- o.cmdheight = 0
+-- -- o.cursorcolumn = true
+-- -- o.cmdheight = 0
 o.expandtab = true
-o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-o.foldcolumn = '0'
+-- -- o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+-- o.foldcolumn = '0'
 o.foldenable = false
-o.foldlevel = 99
-o.foldlevelstart = 99
-o.foldmethod = 'indent'
-o.foldexpr = 'nvim_treesitter#foldexpr()'
-o.hidden = true
-o.hlsearch = true
-o.ignorecase = false
-o.incsearch = true
-o.iskeyword:append('-')
+-- o.foldlevel = 99
+-- o.foldlevelstart = 99
+-- o.foldmethod = 'indent'
+-- o.foldexpr = 'nvim_treesitter#foldexpr()'
+-- o.iskeyword:append('-')
 o.laststatus = 3
-o.list = false
--- o.listchars = [[tab:▏,multispace:▏   ]]
 o.mouse = 'a'
-o.number = false
+-- o.number = false
 o.scrolloff = 8
-o.sessionoptions:append('globals', 'tabpages')
+-- -- o.sessionoptions:append('globals', 'tabpages')
 o.shiftround = true
 o.shiftwidth = 4
 o.showcmd = false
-o.showmatch = true
+-- o.showmatch = true
 o.shortmess:append('I')
 o.shortmess:append('S')
 o.shortmess:append('s')
 o.showmode = false
 o.signcolumn = 'auto'
-o.smartcase = false
-o.softtabstop = 4
+-- o.smartcase = false
+-- o.softtabstop = 4
+-- o.softtabstop
 o.splitbelow = true
 o.splitright = true
 o.swapfile = false
 o.tabstop = 4
-o.updatetime = WINDOWS and 1000 or 600
-o.undodir = vim.fn.stdpath('data') .. '/undodir'
+o.updatetime = 1000
+-- o.updatetime = WINDOWS and 1000 or 600
+-- -- o.undodir = vim.fn.stdpath('data') .. '/undodir'
 o.undofile = true
 o.writebackup = false
